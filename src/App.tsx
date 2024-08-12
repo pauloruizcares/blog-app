9121ff74d4d4fc08d11f7af73bd37c0a819189fe
Paulo Ruiz <paulo.ruiz@mercadolibre.cl>
2024-08-12 19:50:02 -0400

    :sparkles: refactor: changes component to class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,31 @@
 import './App.scss';
 import { Layout } from 'antd';
 import NavBar from "./components/NavBar";
-import { BrowserRouter } from 'react-router-dom'
-import { RouterContainer } from "./routes/Route";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ProtectedRoute from "./components/ProtectedRoute";
+import AddBlog from "./pages/blog/Add";
+import UpdateBlog from "./pages/blog/Update";
+import { ListBlog } from "./pages/blog/List";
+import Home from "./pages/Home";
 
 const { Header, Content } = Layout;
 
 const App = () => {
   return (<>
     <BrowserRouter>
-        <Layout>
-          <Header>
-            <NavBar />
-          </Header>
-          <Content style={{ padding: '0 50px' }}>
-            <RouterContainer />
-          </Content>
-        </Layout>
+      <Layout>
+        <Header>
+          <NavBar />
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blog/add" element={<ProtectedRoute element={<AddBlog />} />} />
+            <Route path="/blog/edit/:id" element={<ProtectedRoute element={<UpdateBlog />} />} />
+            <Route path="/blog" element={<ProtectedRoute element={<ListBlog />} />} />
+          </Routes>
+        </Content>
+      </Layout>
     </BrowserRouter>
 
   </>
--- src/services/BlogService.ts
+++ src/services/BlogService.ts
@@ -4,8 +4,6 @@ import { BlogPost } from "../models/BlogPost";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
-console.log('BASE_URL', BASE_URL);
-
 export const api = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
